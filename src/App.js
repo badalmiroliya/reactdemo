84ec9798baeb54527613c1bb043cf24f44baae77
chanchal <chanchal.kataria@gmail.com>
2021-06-08 23:27:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Button,Container,Row,Col} from 'react-bootstrap';
 import './App.css';
+import User from './User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/home.js';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.textRef=React.createRef();
+    this.state={
+      users:null,
+      toggle: true
+    }
+  }
+  componentDidMount(){
+    fetch('https://reqres.in/api/users').then((resp)=>{
+      resp.json().then((result) =>{
+        this.setState({users:result.data})
+      })
+    })
+  }
+  editVal(){
+      //console.warn(this.textRef);
+      this.textRef.current.focus();
+      this.textRef.current.value="10000";
+  }
+  render(){
+    return (<Home/>);
+    /* return(
+      <div className="App">
+        <h1>Fetch Data From Api</h1>
+        <Container>
+        {
+          this.state.users ?
+          this.state.users.map((item,i) =>
+            <Row>
+              <Col>{i}</Col>
+              <Col>{item.first_name} {item.last_name}</Col>
+              <Col>{item.email}</Col>
+            </Row>
+          )
+          :null
+        }
+          
+        </Container>
+        
+        <h1>User toggle (componentWillUnmount)</h1>
+        {
+          this.state.toggle?
+          <User />
+          : null
+        }
+        <Button onClick={()=>{this.setState({ toggle:!this.state.toggle })}}>Delete User</Button>
+        
+        <h1>What is Ref</h1>
+        <input ref={this.textRef} type="text" name="user" />
+        <button onClick={()=>this.editVal()} >Click Me</button>
+    
+      
+      </div>
+      
+    ); */
+  }
 }
 
 export default App;
